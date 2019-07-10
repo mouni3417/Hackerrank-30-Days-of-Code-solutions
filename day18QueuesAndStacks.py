6154a2cc77194deb642b1b81c181c68dd351df56
Saad <saadbin.sajjad@stud.tu-darmstadt.de>
2019-07-10 19:27:12 +0200

    day 17,18,19 altogether...did after a break

---
+++ day18QueuesAndStacks.py
@@ -0,0 +1,50 @@
+from collections import deque
+
+que = deque([])
+stck = []
+
+
+class Solution:
+    # Write your code here
+
+    def pushCharacter(self, ch):
+        # self.ch = ch
+        stck.append(ch)
+
+    def popCharacter(self):
+        # self.ch = ch
+        return stck.pop()
+
+    def enqueueCharacter(self, ch):
+        que.append(ch)
+
+    def dequeueCharacter(self):
+        return que.popleft()
+
+
+# read the string s
+s = input()
+# Create the Solution class object
+obj = Solution()
+
+l = len(s)
+# push/enqueue all the characters of string s to stack
+for i in range(l):
+    obj.pushCharacter(s[i])
+    obj.enqueueCharacter(s[i])
+
+isPalindrome = True
+'''
+pop the top character from stack
+dequeue the first character from queue
+compare both the characters
+'''
+for i in range(l // 2):
+    if obj.popCharacter() != obj.dequeueCharacter():
+        isPalindrome = False
+        break
+# finally print whether string s is palindrome or not.
+if isPalindrome:
+    print("The word, " + s + ", is a palindrome.")
+else:
+    print("The word, " + s + ", is not a palindrome.")
